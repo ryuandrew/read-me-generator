7fe7ecdc60002fb256ea3be0988a884ae95d10d8
Andrew Ryu <andrewryu@outlook.com>
2022-04-21 18:00:55 -0700

    resolved license issue and updated README for this hw

--- README.md
+++ README.md
@@ -1,1 +1,81 @@
-# read-me-generator
+# Node.js Homework: Professional README Generator
+
+## Description
+
+This app will generate an automated README.md file I will create a command-line application that dynamically generates a professional README.md file from a user's input using the [Inquirer package](https://www.npmjs.com/package/inquirer), File system (FS), and Node.js. 
+
+The application will be invoked by using the following command:
+
+```bash
+node index.js
+```
+
+## User Story
+
+```md
+AS A developer
+I WANT a README generator
+SO THAT I can quickly create a professional README for a new project
+```
+
+## Installation
+```
+User must install Node.js. To run node, Visual Studio Code with Gitbash/Command line interface is required. Then, run the following commands to include 'package.json':
+* npm init -y
+* npm i inquirer
+```
+
+## Usage
+
+```
+To use the README generator, run the command 'node.js' in terminal and answer all the questions. After answering the prompts, 'Your README file has been generated' message will display and the README.md file is found under the same working directory. 
+
+```
+
+## Acceptance Criteria
+
+```md
+GIVEN a command-line application that accepts user input
+
+WHEN I am prompted for information about my application repository
+THEN a high-quality, professional README.md is generated with the title of my project and sections entitled Description, Table of Contents, Installation, Usage, License, Contributing, Tests, and Questions  ✅
+
+
+WHEN I enter my project title
+THEN this is displayed as the title of the README  ✅
+
+
+WHEN I enter a description, installation instructions, usage information, contribution guidelines, and test instructions
+THEN this information is added to the sections of the README entitled Description, Installation, Usage, Contributing, and Tests  ✅
+
+
+WHEN I choose a license for my application from a list of options
+THEN a badge for that license is added near the top of the README and a notice is added to the section of the README entitled License that explains which license the application is covered under  ✅
+
+
+WHEN I enter my GitHub username
+THEN this is added to the section of the README entitled Questions, with a link to my GitHub profile
+
+
+WHEN I enter my email address
+THEN this is added to the section of the README entitled Questions, with instructions on how to reach me with additional questions  ✅
+
+
+WHEN I click on the links in the Table of Contents
+THEN I am taken to the corresponding section of the README  ✅
+```
+
+## License
+
+[![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)](https://img.shields.io/badge/License-MIT-yellow.svg)
+
+
+## Screenshot
+
+
+---
+
+#### Github: https://github.com/ryuandrew/read-me-generator
+
+
+
--- index.js
+++ index.js
@@ -14,76 +14,105 @@ inquirer
     name: "description"
     },
     {type: "input",
-    message: "?",
+    message: "What are your installation commands?",
     name: "installation"
     },
     {type: "input",
-    message: "?",
+    message: "How do you use your app?",
     name: "usage"
     },
-    {type: "input",
-    message: "?",
-    name: "lincense"
+    {type: "checkbox",
+    message: "Choose a license for your application",
+    name: "license",
+    choices: [
+        "MIT",
+        "Apache",
+        "GPL v2",
+        "GPL v3",
+        "N/A"]
     },
     {type: "input",
-    message: "?",
-    name: "contributor"
+    message: "How can a user contribute to your project?",
+    name: "contributing"
     },
     {type: "input",
-    message: "?",
-    name: ""
+    message: "Test instructions",
+    name: "test"
     },
     {type: "input",
-    message: "?",
-    name: ""
+    message: "What is your GitHub username?",
+    name: "userName"
     },
     {type: "input",
-    message: "?",
-    name: ""
-    },
-    {type: "input",
-    message: "?",
-    name: ""
+    message: "What is your email?",
+    name: "email"
     },
   ])
   .then((answers) => {
-    // Use user feedback for... whatever!!
-    const content = `<!DOCTYPE html>
-    <html lang="en">
-    <head>
-        <meta charset="UTF-8">
-        <meta http-equiv="X-UA-Compatible" content="IE=edge">
-        <meta name="viewport" content="width=device-width, initial-scale=1.0">
-        <title>Group Project</title>
-        <link rel="stylesheet" href="./style.css">
-    </head>
-    <body>
-        <main>
-            <h1>My name</h1>
-            <p>${answers.yourName}</p>
-            <h1>My location</h1>
-            <p>${answers.location}</p>
-            <h1>My bio</h1>
-            <p>${answers.yourBio}</p>
-            <h1>My LinkedIn Profile</h1>
-            <a href="${answers.linkedIn}">${answers.linkedIn}</a>
-            <h1>My GitHub Profile</h1>
-            <a href="${answers.gitHub}">${answers.gitHub}</a>
-        </main>
-    </body>
-    </html>`
-    fs.writeFile(`./${answers.yourName}.html`, content, err => {
+    var badge = " "
+    if (answers.license[0] === "MIT") {
+      badge = "[![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)](https://img.shields.io/badge/License-MIT-yellow.svg)"
+    } else if (answers.license[0] === "Apache") {
+      badge = "[![License](https://img.shields.io/badge/License-Apache%202.0-blue.svg)](https://img.shields.io/badge/License-Apache%202.0-blue.svg)"
+    } else if (answers.license[0] === "GPL v2") {
+      badge = "[![License: GPL v2](https://img.shields.io/badge/License-GPL%20v2-blue.svg)](https://img.shields.io/badge/License-GPL%20v2-blue.svg)"
+    } else {
+      badge = "[![License: GPL v3](https://img.shields.io/badge/License-GPLv3-blue.svg)](https://img.shields.io/badge/License-GPLv3-blue.svg)"
+    }
+
+    const content = `# ${answers.title}
+## Description
+${answers.description}
+
+---
+## Table of Contents
+* [Installation](#installation)
+* [Usage](#usage)
+* [License](#license)
+* [Contributin](#contributing)
+* [Tests](#test)
+* [Questions](#questions)
+
+---
+## Installation
+To install the necessary dependencies, follow the instructions:
+
+${answers.installation}
+
+---
+## Usage
+${answers.usage}
+
+---
+## License <a name = "license"></a>
+This project is licensed under:  
+${badge}
+
+---
+## Contributing
+To contribute to this app, 
+* Fork the repo
+* Clone the repo to your computer \`\`\`git clone URL\`\`\`
+* Create a new branch \`\`\`git checkout -b NAME-TASK\`\`\`
+* Commit changes \`\`\`git commit -m "OVERVIEWOFACTION"\`\`\`
+* Push your branch \`\`\`git push\`\`\`
+* Create a pull request
+
+---
+## Test
+Update tests when appropriate.
+${answers.test}
+
+---
+## Questions
+If you have any questions feel free to contact me at:
+* GithHub: https://github.com/${answers.userName}
+* E-mail: ${answers.email}
+    `
+    fs.writeFile(`./${answers.title}README.md`, content, err => {
         if(err) {
             console.error(err)
             return
-        }
+        } console.log("Your README file has been generated")
     })
   })
-
-//   table of content
-//   installation
-//   usage
-//   licenste
-//   contributing
-//   test 
-//   questions
